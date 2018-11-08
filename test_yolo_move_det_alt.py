10c431dfffd67e177215244072b68017c4dac032
lavabar <lavabarser@gmail.com>
2018-11-08 21:14:18 +0300

    yolo_move_det_alt better for multiple objs

--- test_yolo_move_det_alt.py
+++ test_yolo_move_det_alt.py
@@ -3,6 +3,11 @@ import cv2
 from skimage.measure import compare_ssim
 import imutils
 
+from darkflow.net.build import TFNet
+
+options = {"model": "yolov2-tiny.cfg", "load": "yolov2-tiny.weights", "threshold": 0.1, "labels": "labels.txt"}
+
+tfnet = TFNet(options)
 
 def get_img_objs(original, frame):
     res = frame.copy()
@@ -15,29 +20,30 @@ def get_img_objs(original, frame):
     if score > 0.90:
         return res
     diff = (diff * 255).astype("uint8")
-
     thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
     
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-    
-    # loop over the contours
-    for c in cnts:
-    	# compute the bounding box of the contour and then draw the
-    	# bounding box on both input images to represent where the two
-    	# images differ
-        (x, y, w, h) = cv2.boundingRect(c)
+    result = tfnet.return_predict(frame)
 
-        #if w * h < 3000:
-        #    continue
+    for obj in result:
+        if obj['label'] == 'person' and obj['confidence'] >= 0.7:
+            x1 = obj['topleft']['x']
+            y1 = obj['topleft']['y']
+            x2 = obj['bottomright']['x']
+            y2 = obj['bottomright']['y']
+            
+            cut = thresh[y1:y2, x1:x2]
+            w = x2 - x1
+            h = y2 - y1
+            pts = np.count_nonzero(cut)
+            if pts > w * h * 0.4:
+                cv2.rectangle(res, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
-        cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return res
 
 cap = cv2.VideoCapture(0)
 
 ret, original = cap.read()
-for _ in range(100):
+for _ in range(50):
     ret, original = cap.read()
 
 while(True):
--- test_yolo_realtime.py
+++ test_yolo_realtime.py
@@ -6,22 +6,24 @@ options = {"model": "yolov2-tiny.cfg", "load": "yolov2-tiny.weights", "threshold
 tfnet = TFNet(options)
 
 cap = cv2.VideoCapture(0)
-
+counter = 0
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    counter += 1
+    if counter == 10:
+        result = tfnet.return_predict(frame)
+    #    print(result)
 
-    result = tfnet.return_predict(frame)
-#    print(result)
-
-    for obj in result:
-        if obj['label'] == 'person' and obj['confidence'] >= 0.7:
-            x1 = obj['topleft']['x']
-            y1 = obj['topleft']['y']
-            x2 = obj['bottomright']['x']
-            y2 = obj['bottomright']['y']
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
+        for obj in result:
+            if obj['label'] == 'person' and obj['confidence'] >= 0.85:
+                x1 = obj['topleft']['x']
+                y1 = obj['topleft']['y']
+                x2 = obj['bottomright']['x']
+                y2 = obj['bottomright']['y']
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        counter = 0
+    
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
