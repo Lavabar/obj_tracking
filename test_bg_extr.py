5af54d1c88878320763d4f395b0c1e97663ce68d
lavabar <lavabarser@gmail.com>
2018-11-15 11:55:03 +0300

    trying to optimize bg_extractor

--- test_bg_extr.py
+++ test_bg_extr.py
@@ -1,24 +1,34 @@
 import cv2
 import numpy as np
+from scipy.ndimage import median_filter
 
-N_FRAMES = 10
-#BACKGROUND = np.asarray([0, 200, 0])
-#FOREGROUND = np.asarray([0, 200, 0])
-FOREGROUND = 0
+N_FRAMES = 100
 
 global stats, n
 stats = np.zeros((480, 640, 256))
 n = 0
 
-def update_stats(img):
+def update_bg(img, bg):
 
     global stats, n
-    
+
+    # TODO - optimization
     for y in range(480):
         for x in range(640):
             stats[y, x, img[y, x]] += 1
     
     n += 1
+    
+    # alternative variant (slower)
+    #img = to_categorical(img, num_classes=256)
+    #stats = np.add(stats, img)
+    
+    if n == N_FRAMES:
+        bg = np.argmax(stats, axis=2)
+        stats = np.zeros((480, 640, 256))
+        n = 0
+        
+    return bg
 
 
 cap = cv2.VideoCapture(0)
@@ -30,15 +40,10 @@ if __name__ == "__main__":
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        update_stats(gray)
-        if n == N_FRAMES:
+        bg = update_bg(gray, bg)
 
-            bg = np.argmax(stats, axis=2)
-            stats = np.zeros((480, 640, 256))
-            n = 0
-
-
-        cv2.imshow("frame", np.array(bg, np.uint8))
+        cv2.imshow("bg", median_filter(np.array(bg, np.uint8), 3))
+        cv2.imshow("frame", gray)
 
         k = cv2.waitKey(30) & 0xff
         if k == 27:
